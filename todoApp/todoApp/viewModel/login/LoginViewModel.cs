6e7706b91b066fa8d72a3ca7f442d69d4f492e21
2KB\kbin9919 <osl71297264@gmail.com>
2025-11-04 21:44:01 +0900

    no message

---
+++ todoApp/todoApp/viewModel/login/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todoApp.viewModel.login
+{
+    public class LoginViewModel
+    {
+       
+    }
+}
